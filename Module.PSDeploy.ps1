f700a59fa330a9cb813b9ab142da0ffe8ec064a1
Mark Kraus <github@markekraus.com>
2017-11-29 13:38:55 -0600

    !Deploy

--- Module.PSDeploy.ps1
+++ Module.PSDeploy.ps1
@@ -1,17 +1,17 @@
 Deploy Module {
-    #By PSGalleryModule {
-    #    FromSource "bin/$ENV:ModuleName"
-    #    To PSGallery
-    #    WithOptions @{
-    #        ApiKey = $ENV:NugetApiKey
-    #    }
-    #}
-
     By AppVeyorModule {
         FromSource "bin/$ENV:ModuleName"
         To AppVeyor
         WithOptions @{
             Version = $env:APPVEYOR_BUILD_VERSION
+        }
+    }
+
+    By PSGalleryModule {
+        FromSource "bin/$ENV:ModuleName"
+        To PSGallery
+        WithOptions @{
+            ApiKey = $ENV:NugetApiKey
         }
     }
 }
