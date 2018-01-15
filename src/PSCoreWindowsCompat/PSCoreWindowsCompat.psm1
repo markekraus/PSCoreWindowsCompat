31b01a80a4a01e7c52180f3685a7c17a4337b372
Mark Kraus <github@markekraus.com>
2018-01-15 08:16:44 -0600

    Add ADSI Searcher

--- src/PSCoreWindowsCompat/PSCoreWindowsCompat.psm1
+++ src/PSCoreWindowsCompat/PSCoreWindowsCompat.psm1
@@ -1,3 +1,4 @@
 # Add ADSI Type Accelerator
 $TypeAccelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
 $TypeAccelerators::Add('ADSI', [System.DirectoryServices.DirectoryEntry])
+$TypeAccelerators::Add('ADSISearcher', [System.DirectoryServices.DirectorySearcher])
